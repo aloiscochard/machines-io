c8f30d262f6801c2bd5dc8b10d1acea8fca910eb
Alois Cochard <alois.cochard@gmail.com>
2014-07-06 00:33:26 +0100

    Add `sourceFile` and remove `*ByLine` functions.

--- src/System/IO/Machine.hs
+++ src/System/IO/Machine.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE DoAndIfThenElse #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE KindSignatures #-}
 module System.IO.Machine where
 
 import Control.Monad (when)
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.IOData (IOData, hGetLine, hPutStrLn)
 import Data.Machine
 import System.IO (Handle, IOMode(ReadMode), hClose, hIsEOF, openBinaryFile)
 
 -- TODO Test coverage
--- TODO Is it correct to use openBinaryFIle in sourceFileWith?
+-- TODO Is it correct to use openBinaryFile in sourceFileWith?
 -- alternatively, one can just use System.IO.withFile
 
 data IODataMode a = IODataMode (Handle -> IO a) (Handle -> a -> IO ())
@@ -20,7 +21,7 @@ type IOSource a = forall k. MachineT IO k a
 byLine :: IOData a => IODataMode a
 byLine = IODataMode hGetLine hPutStrLn
 
-sourceIO :: IO r -> (r -> IO Bool) -> (r -> IO a) -> IOSource a
+sourceIO :: MonadIO m => m r -> (r -> m Bool) -> (r -> m a) -> forall k. MachineT m k a
 sourceIO acquire release read = MachineT $ do
   r         <- acquire
   released  <- release r
@@ -30,23 +31,14 @@ sourceIO acquire release read = MachineT $ do
     x <- read r
     return . Yield x $ sourceIO acquire release read
 
+sourceFile :: IODataMode a -> FilePath -> IOSource a
+sourceFile (IODataMode r _) = sourceFileWith r
+
 sourceHandle :: IODataMode a -> Handle -> IOSource a
-sourceHandle (IODataMode r _) h = sourceIO (return h) hIsEOF r
-
-sourceHandleWith :: (Handle -> IO a) -> Handle -> IOSource a
-sourceHandleWith f h = sourceIO (return h) hIsEOF f
-
-sourceHandleByLine :: IOData a => Handle -> IOSource a
-sourceHandleByLine = sourceHandleWith hGetLine
+sourceHandle (IODataMode r _) = sourceHandleWith r
 
 sinkHandle :: IOData a => IODataMode a -> Handle -> IOSink a
 sinkHandle (IODataMode _ w) h = repeatedly $ await >>= \x -> liftIO $ w h x
-
-sinkHandleWith :: IOData a => (Handle -> a -> IO ()) -> Handle -> IOSink a
-sinkHandleWith f h = repeatedly $ await >>= \x -> liftIO $ f h x
-
-sinkHandleByLine :: IOData a => Handle -> IOSink a
-sinkHandleByLine = sinkHandleWith hPutStrLn
 
 sourceFileWith :: (Handle -> IO a) -> FilePath -> IOSource a
 sourceFileWith f fp = sourceIO acquire release f where
@@ -56,5 +48,8 @@ sourceFileWith f fp = sourceIO acquire release f where
     when isEOF $ hClose h
     return isEOF
 
-sourceFileByLine :: IOData a => FilePath -> IOSource a
-sourceFileByLine fp = sourceFileWith hGetLine fp
+sourceHandleWith :: (Handle -> IO a) -> Handle -> IOSource a
+sourceHandleWith f h = sourceIO (return h) hIsEOF f
+
+sinkHandleWith :: IOData a => (Handle -> a -> IO ()) -> Handle -> IOSink a
+sinkHandleWith f h = repeatedly $ await >>= \x -> liftIO $ f h x
