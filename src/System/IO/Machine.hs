d91e734fc56d8bc60cb48ef0d39fe4c425ddf6d9
Alois Cochard <alois.cochard@gmail.com>
2014-07-13 12:44:47 +0100

    Refactor module by abstracting over `IO` monad using `MonadIO` typeclass.

--- src/System/IO/Machine.hs
+++ src/System/IO/Machine.hs
@@ -9,32 +9,44 @@ import Data.IOData (IOData, hGetLine, hPutStrLn)
 import Data.Machine
 import System.IO (Handle, IOMode(ReadMode), hClose, hIsEOF, openBinaryFile)
 
-data IODataMode a = IODataMode (Handle -> IO a) (Handle -> a -> IO ())
+type DataModeIO m a = MonadIO m => ((Handle -> m a), (Handle -> a -> m ()))
+type SinkIO m k = MonadIO m => forall a. ProcessT m k a
+type SourceIO m a = MonadIO m => forall k. MachineT m k a
 
+type IODataMode a = ((Handle -> IO a), (Handle -> a -> IO ()))
 type IOSink k = forall a. ProcessT IO k a
 type IOSource a = forall k. MachineT IO k a
 
-byLine :: IOData a => IODataMode a
-byLine = IODataMode hGetLine hPutStrLn
+byLine :: IOData a => DataModeIO m a
+byLine = (hGetLine, hPutStrLn)
 
-sourceIO :: MonadIO m => m r -> (r -> m Bool) -> (r -> m a) -> forall k. MachineT m k a
-sourceIO acquire release read = MachineT $ do
+sourceIO :: IO a -> SourceIO m a
+sourceIO f = repeatedly $ liftIO f >>= yield
+
+sourceHandle :: DataModeIO m a -> Handle -> SourceIO m a
+sourceHandle (r, _) = sourceHandleWith r
+
+sourceIOWith :: m r -> (r -> m Bool) -> (r -> m a) -> SourceIO m a
+sourceIOWith acquire release read = MachineT $ do
   r         <- acquire
   released  <- release r
   if released then
     return Stop
   else do
     x <- read r
-    return . Yield x $ sourceIO acquire release read
+    return . Yield x $ sourceIOWith acquire release read
 
-sourceHandle :: IODataMode a -> Handle -> IOSource a
-sourceHandle (IODataMode r _) = sourceHandleWith r
+sourceHandleWith :: (Handle -> m a) -> Handle -> SourceIO m a
+sourceHandleWith f h = sourceIOWith (return h) (liftIO . hIsEOF) f
 
-sinkHandle :: IOData a => IODataMode a -> Handle -> IOSink a
-sinkHandle (IODataMode _ w) h = repeatedly $ await >>= \x -> liftIO $ w h x
+sinkIO :: (a -> IO ()) -> SinkIO m a
+sinkIO f = repeatedly $ await >>= liftIO . f
 
-sourceHandleWith :: (Handle -> IO a) -> Handle -> IOSource a
-sourceHandleWith f h = sourceIO (return h) hIsEOF f
+sinkHandle :: IOData a => IODataMode a -> Handle -> SinkIO m a
+sinkHandle (_, w) h = repeatedly $ await >>= liftIO . w h
 
-sinkHandleWith :: IOData a => (Handle -> a -> IO ()) -> Handle -> IOSink a
-sinkHandleWith f h = repeatedly $ await >>= \x -> liftIO $ f h x
+sinkHandleWith :: IOData a => (Handle -> a -> IO ()) -> Handle -> SinkIO m a
+sinkHandleWith f h = repeatedly $ await >>= liftIO . f h
+
+printer :: Show a => SinkIO m a
+printer = sinkIO $ liftIO . print
